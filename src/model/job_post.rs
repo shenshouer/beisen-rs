4a143cc64e98a4135e2f52dd271ca8f66042acb0
Sope Shen <shenshouer51@gmail.com>
2022-11-03 19:03:21 +0800

    修复获取职级与职位信息问题

--- src/model/job_post.rs
+++ src/model/job_post.rs
@@ -1,18 +1,9 @@
-use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
-
-use crate::utils::datetime::{self, date_format};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct JobPost {
     #[serde(rename = "Code")]
     pub code: String,
-    #[serde(
-        default = "datetime::default",
-        rename = "CreatedTime",
-        with = "date_format"
-    )]
-    pub created_time: DateTime<Local>,
     #[serde(rename = "Name")]
     pub name: String,
     #[serde(rename = "OId")]
@@ -21,8 +12,6 @@ pub struct JobPost {
     pub oid_job_level_type: String,
     #[serde(rename = "Status")]
     pub status: i32,
-    // #[serde(rename = "StdIsDeleted")]
-    // pub std_is_deleted: bool,
-    #[serde(rename = "_id")]
-    pub id: String,
+    #[serde(rename = "IsDeleted")]
+    pub is_deleted: bool,
 }
--- src/model/setting.rs
+++ src/model/setting.rs
@@ -1,7 +1,4 @@
-use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
-
-use crate::utils::datetime::{self, date_format};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
@@ -11,24 +8,18 @@ pub enum Setting {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct JobLevel {
-    #[serde(
-        default = "datetime::default",
-        rename = "CreatedTime",
-        with = "date_format"
-    )]
-    pub created_time: DateTime<Local>,
-    #[serde(rename = "IsDeleted")]
-    pub is_deleted: bool,
     #[serde(rename = "Level")]
     pub level: i32,
     #[serde(rename = "Name")]
     pub name: String,
+    // #[serde(rename = "NameEnUS")]
+    // pub name_en_us: String,
     #[serde(rename = "OId")]
     pub oid: String,
     #[serde(rename = "Status")]
     pub status: i32,
-    // #[serde(rename = "StdIsDeleted")]
-    // pub std_is_deleted: bool,
-    #[serde(rename = "_id")]
-    pub id: String,
+    #[serde(rename = "StdIsDeleted")]
+    pub std_is_deleted: bool,
+    // #[serde(rename = "_id")]
+    // pub id: String,
 }
